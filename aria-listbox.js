0a675e4be55f458cb140dff0d219d5ce918f3486
Nathan Black <nathan@nathanblack.org>
2015-10-28 01:43:10 -0700

    First initial tests

--- aria-listbox.js
+++ aria-listbox.js
@@ -8,6 +8,22 @@
   }
 })(function() {
 
-  return function AriaListbox() {}
+  return function AriaListbox(el) {
+    if (!(el instanceof HTMLElement)) {
+      throw new Error('The listbox container element must be the first parameter to AriaListbox')
+    }
+
+    el.setAttribute('role', 'aria-listbox')
+
+    var firstSelected = el.querySelector('[aria-selected="true"]')
+    if (firstSelected) {
+      firstSelected.setAttribute('tabindex', '0')
+    } else {
+      var options = el.querySelectorAll('[role="option"]')
+      if (options.length) {
+        options[0].setAttribute('tabindex', '0')
+      }
+    }
+  }
 
 })
--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,16 @@
 describe('aria-listbox', function() {
-  var testEl, testSetup = function() {
+  var testEl, listbox, testSetup = function(opts) {
+    opts = opts || {}
+    testEl = document.createElement(opts.tag || 'ul')
+    testEl.innerHTML = opts.innerHTML ||
+      ['cherry', 'orange', 'banana', 'watermelon', 'pineapple'].map(function(fruit) {
+        return '<li role="option"><span>' + fruit + '<span></li>'
+      })
+    if (opts.selected) {
+      testEl.children[opts.selected].setAttribute('aria-selected', 'true')
+    }
+
+    listbox = new AriaListbox(testEl)
   },
   click = function(el) {
     var evt = document.createEvent('MouseEvents')
@@ -20,14 +31,31 @@ describe('aria-listbox', function() {
     })
   }
 
-  afterEach(function() {
-    testEl && testEl.parentElement.removeChild(testEl)
-    testEl = null
+  it('should throw if not provided a container element', function() {
+    (function() {
+      new AriaListbox()
+    }).should.throw('The listbox container element must be the first parameter to AriaListbox')
   })
 
-  it('should add the aria-listbox on the element')
-  it('should add aria-option elements to direct li descendants if the element is ol or ul')
-  it('should throw if there are no aria-option descendants on a non-list element')
-  it('should make the element tabbable')
-  it('should set aria-select on an option when it is clicked')
+  it('should add [role="aria-listbox"] on the element', function() {
+    testSetup()
+    testEl.should.have.attribute('role', 'aria-listbox')
+  })
+
+  it('should make the first aria-selected option tabbable if available, else the first role="option"', function() {
+    testSetup()
+    attributesFor('li', 'tabindex').should.deep.equal(['0', null, null, null, null])
+
+    testSetup({ selected: 2 })
+    attributesFor('li', 'tabindex').should.deep.equal([null, null, '0', null, null])
+  })
+  it('should set focus and tabindex=0 on the next role="option" element on arrow down, if it exists')
+  it('should set focus and tabindex=0 on the previous role="option" element on arrow up, if it exists')
+  it('should allow next and previous keys to be configured, allowing multiple mappings')
+  it('should set aria-selected on an option when it is clicked')
+  it('should set aria-selected on an option when a child element of it is clicked')
+  it('should set aria-selected on the focused option when space or enter is pressed')
+  it('should allow the select keys to be configured')
+  it('should allow multiple items to be selected by mouse when aria-multiselect is true')
+  it('should allow multiple items to be selected by keyboard when aria-multiselect is true')
 })
