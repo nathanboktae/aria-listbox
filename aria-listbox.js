4011511e4d0f16019f6676565224ed1f9d124041
Nathan Black <nathan@nathanblack.org>
2015-10-29 00:07:17 -0700

    Implement keyboard functionality

--- aria-listbox.js
+++ aria-listbox.js
@@ -8,12 +8,25 @@
   }
 })(function() {
 
-  return function AriaListbox(el) {
+  return function AriaListbox(el, opts) {
     if (!(el instanceof HTMLElement)) {
       throw new Error('The listbox container element must be the first parameter to AriaListbox')
     }
 
+    function normalizeKeyOptions(keys, defaults) {
+      if (!Array.isArray(keys)) {
+        keys = keys ? [keys] : defaults
+      }
+      return keys.map(function(k) {
+        return typeof k === 'string' && !Number(k) ? k.charCodeAt(0) : Number(k)
+      })
+    }
+
     el.setAttribute('role', 'aria-listbox')
+    opts = opts || {}
+    opts.prevKeys = normalizeKeyOptions(opts.prevKeys, [38])
+    opts.nextKeys = normalizeKeyOptions(opts.nextKeys, [40])
+    opts.selectKeys = normalizeKeyOptions(opts.selectKeys, [13, 32])
 
     var firstSelected = el.querySelector('[aria-selected="true"]')
     if (firstSelected) {
@@ -23,6 +36,12 @@
       if (options.length) {
         options[0].setAttribute('tabindex', '0')
       }
+    }
+
+    function clearTabIndexes() {
+      Array.prototype.forEach.call(el.querySelectorAll('[tabindex]'), function(n) {
+        n.removeAttribute('tabindex')
+      })
     }
 
     function select(child) {
@@ -35,21 +54,48 @@
         child.removeAttribute('aria-selected')
         return
       }
-      Array.prototype.forEach.call(el.querySelectorAll('[tabindex]'), function(n) {
-        n.removeAttribute('tabindex')
-      })
+      clearTabIndexes()
       child.setAttribute('aria-selected', 'true')
       child.setAttribute('tabindex', '0')
     }
 
-    el.addEventListener('click', function(e) {
+    function optionNode(e) {
       var optionEl = e.target
       while (optionEl && optionEl.getAttribute('role') !== 'option') {
         optionEl = optionEl.parentElement
       }
+      return optionEl
+    }
 
+    el.addEventListener('click', function(e) {
+      var optionEl = optionNode(e)
       if (optionEl) {
         select(optionEl)
+      }
+    })
+
+    function handleKey(optionEl, code, keys, delta) {
+      if (keys.indexOf(code) !== -1) {
+        var optionEls = el.querySelectorAll('[role="option"]'),
+            idx = Array.prototype.indexOf.call(optionEls, optionEl)
+
+        var next = optionEls[idx + delta]
+        if (next) {
+          clearTabIndexes()
+          next.setAttribute('tabindex', 0)
+          next.focus()
+        }
+      }
+    }
+
+    el.addEventListener('keydown', function(e) {
+      var optionEl = optionNode(e),
+          code = e.keyCode || e.code
+      if (opts.selectKeys.indexOf(code) !== -1) {
+        select(optionEl)
+      } else if (e.target && e.target.getAttribute('role') === 'option') {
+        handleKey(optionEl, code, opts.nextKeys, 1)
+        handleKey(optionEl, code, opts.prevKeys, -1)
       }
     })
   }
--- tests/tests.js
+++ tests/tests.js
@@ -13,7 +13,8 @@ describe('aria-listbox', function() {
       testEl.children[opts.selected].setAttribute('aria-selected', 'true')
     }
 
-    listbox = new AriaListbox(testEl)
+    document.body.appendChild(testEl)
+    listbox = new AriaListbox(testEl, Object.keys(opts).length && opts)
   },
   click = function(el) {
     var evt = document.createEvent('MouseEvents')
@@ -22,6 +23,19 @@ describe('aria-listbox', function() {
       el = testEl.querySelector(el)
     }
     el.dispatchEvent(evt)
+    return evt
+  },
+  keydown = function(el, key) {
+    // Trying to "properly" create a KeyboardEvent is a huge bag of hurt
+    var evt = document.createEvent('UIEvent')
+
+    evt.initEvent('keydown', true, true)
+    evt.keyCode = evt.code = typeof key === 'string' ? key.charCodeAt(0) : key
+    if (typeof el === 'string') {
+      el = testEl.querySelector(el)
+    }
+    el.dispatchEvent(evt)
+    return evt
   },
   textNodesFor = function(selector) {
     return Array.prototype.map.call(testEl.querySelectorAll(selector), function(el) {
@@ -34,6 +48,11 @@ describe('aria-listbox', function() {
       return el.getAttribute(attr)
     })
   }
+
+  afterEach(function() {
+    testEl && testEl.parentElement && testEl.parentElement.removeChild(testEl)
+    testEl = null
+  })
 
   it('should throw if not provided a container element', function() {
     (function() {
@@ -54,9 +73,50 @@ describe('aria-listbox', function() {
     attributesFor('li', 'tabindex').should.deep.equal([null, null, '0', null, null])
   })
 
-  it('should set focus and tabindex=0 on the next role="option" element on arrow down, if it exists')
-  it('should set focus and tabindex=0 on the previous role="option" element on arrow up, if it exists')
-  it('should allow next and previous keys to be configured, allowing multiple mappings')
+  it('should set focus and tabindex=0 on the next role="option" element on arrow down, if it exists', function() {
+    testSetup()
+    keydown('li:first-child', 40)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['orange'])
+    document.activeElement.textContent.should.equal('orange')
+    keydown(document.activeElement, 40)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['banana'])
+    keydown(document.activeElement, 40)
+    keydown(document.activeElement, 40)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['pineapple'])
+    keydown(document.activeElement, 40)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['pineapple'])
+    document.activeElement.textContent.should.equal('pineapple')
+  })
+
+  it('should set focus and tabindex=0 on the previous role="option" element on arrow up, if it exists', function() {
+    testSetup()
+    keydown('li:nth-child(4)', 38)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['banana'])
+    document.activeElement.textContent.should.equal('banana')
+    keydown(document.activeElement, 38)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['orange'])
+    keydown(document.activeElement, 38)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['cherry'])
+    keydown(document.activeElement, 38)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['cherry'])
+    document.activeElement.textContent.should.equal('cherry')
+  })
+
+  it('should allow next and previous keys to be configured, allowing multiple mappings', function() {
+    testSetup({ prevKeys: [38, 'w'], nextKeys: 's' })
+    keydown('li:nth-child(4)', 's')
+    textNodesFor('[tabindex="0"]').should.deep.equal(['pineapple'])
+    document.activeElement.textContent.should.equal('pineapple')
+    keydown(document.activeElement, 's')
+    textNodesFor('[tabindex="0"]').should.deep.equal(['pineapple'])
+    document.activeElement.textContent.should.equal('pineapple')
+    keydown(document.activeElement, 'w')
+    textNodesFor('[tabindex="0"]').should.deep.equal(['watermelon'])
+    document.activeElement.textContent.should.equal('watermelon')
+    keydown(document.activeElement, 38)
+    textNodesFor('[tabindex="0"]').should.deep.equal(['banana'])
+    document.activeElement.textContent.should.equal('banana')
+  })
 
   it('should set aria-selected on an option when it is clicked, clearing previous selections for single select', function() {
     testSetup()
@@ -72,10 +132,28 @@ describe('aria-listbox', function() {
     selectedNodes().should.deep.equal(['orange'])
   })
 
-  it('should not prevent the default for clicks and keydown events to allow other code to handle them to')
+  it('should not prevent the default for clicks and keydown events to allow other code to handle them to', function() {
+    testSetup()
+    keydown('li:nth-child(4)', 38).defaultPrevented.should.be.false
+    click('li:nth-child(2) span').defaultPrevented.should.be.false
+  })
 
-  it('should set aria-selected on the focused option when space or enter is pressed')
-  it('should allow the select keys to be configured')
+  it('should set aria-selected on the focused option when space or enter is pressed', function() {
+    testSetup()
+    keydown('li:nth-child(4)', 13)
+    selectedNodes().should.deep.equal(['watermelon'])
+    keydown('li:first-child span', 32)
+    selectedNodes().should.deep.equal(['cherry'])
+  })
+
+  it('should allow the select keys to be configured', function() {
+    testSetup({ selectKeys: ['|', 'v'] })
+    keydown('li:nth-child(4)', '|')
+    selectedNodes().should.deep.equal(['watermelon'])
+    keydown('li:first-child span', 'v')
+    selectedNodes().should.deep.equal(['cherry'])
+  })
+
   it('should fire an event when the selection changes')
 
   describe('multiselect', function() {
@@ -97,7 +175,22 @@ describe('aria-listbox', function() {
       selectedNodes().should.deep.equal(['watermelon'])
     })
 
-    it('should allow multiple items to be selected by keyboard')
-    it('should toggle a selected item when selected by keyboard')
+    it('should allow multiple items to be selected by keyboard', function() {
+      testSetup({ multiselect: true })
+      keydown('li:nth-child(4)', 13)
+      selectedNodes().should.deep.equal(['watermelon'])
+      keydown('li:first-child span', 32)
+      selectedNodes().should.deep.equal(['cherry', 'watermelon'])
+    })
+
+    it('should toggle a selected item when selected by keyboard', function() {
+      testSetup({ multiselect: true })
+      keydown('li:first-child', 32)
+      selectedNodes().should.deep.equal(['cherry'])
+      keydown('li:nth-child(3)', 13)
+      selectedNodes().should.deep.equal(['cherry', 'banana'])
+      keydown('li:first-child span', 13)
+      selectedNodes().should.deep.equal(['banana'])
+    })
   })
 })
